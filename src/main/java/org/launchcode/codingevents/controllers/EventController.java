7b01940b4d82062a197a9f2d147bd232e24a3bd9
rania <raniaa13.rh@gmail.com>
2021-10-11 01:50:56 -0500

    finish test

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,5 @@
 package org.launchcode.codingevents.controllers;
-//hi
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
